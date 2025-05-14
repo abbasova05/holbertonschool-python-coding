09866ce7de392a9ca954aa24317435e7aba14c6c
abbasova05 <tahminabbasova05@gmail.com>
2025-05-14 12:19:51 +0000

    tack

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,20 +1,4 @@
 #!/usr/bin/python3
-"""
-This module defines a class Square that represents a geometric square.
-"""
-
-class Square:
-    """
-    Represents a square.
-
-    This class defines a square by its size, which is a private attribute.
-    """
-
+class Square
     def __init__(self, size):
-        """
-        Initializes a new Square instance with a given size.
-
-        Args:
-            size (int): The size of the square.
-        """
         self.__size = size
